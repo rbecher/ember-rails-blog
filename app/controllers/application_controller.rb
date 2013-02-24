532c5a894ad1435be86e9e745e3f1527888dfd6b
Ronald Becher <ronald.becher@gmail.com>
2013-02-24 18:59:10 +0100

    protect params better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def minimum_parameters
+    param.slice(*minimum_parameter_keys)
+  end
+
+  private
+
+  def requires_parameters(*required)
+    required.each do |p|
+      raise EmberBlog::InvalidParameters.new(p) unless params.has_key?(p)
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Post do
+  # TODO add tests
   pending "add some examples to (or delete) #{__FILE__}"
 end
