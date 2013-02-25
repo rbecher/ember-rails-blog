c0c324f9e2a76abd3f96f9d3d21a34e606e99026
Ronald Becher <ronald.becher@gmail.com>
2013-02-25 05:00:28 +0100

    remove useless code and add test file

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -1,13 +1,3 @@
-Then /^the JSON response should have "([^\"]*)" appear (\d+) times?$/ do |json_path, times|
-  json    = JSON.parse(page.driver.last_response.body)
-  results = JsonPath.new(json_path).on(json).to_a
-  if page.respond_to? :should
-    results.size.should == times.to_i
-  else
-    assert_equal times.to_i, results.size
-  end
-end
-
 Then /^show me the response headers$/ do
   puts last_response.headers
   true
