9f28e3e6987bee3af3c77b8e89647e7df379650c
Ronald Becher <ronald.becher@gmail.com>
2013-03-04 10:09:30 +0100

    use correct name for timed attributes

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,6 +1,6 @@
 EmberBlog.Post = DS.Model.extend({
     title: DS.attr('string'),
     body: DS.attr('string'),
-    created_at: DS.attr('date'),
-    updated_at: DS.attr('date')
+    createdAt: DS.attr('date'),
+    updatedAt: DS.attr('date')
 });
