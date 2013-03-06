e4519b818c78f05428d3a0c325e5ceed7f75babe
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-06 15:50:41 +0100

    extend seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Coding: UTF-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -15,5 +16,6 @@ posts = Post.create([
 comments = Comment.create([
     {content: "Ein Kommentar.", post_id: 1},
     {content: "Noch einer.", post_id: 1},
-    {content: "Und noch einer.", post_id: 1}
+    {content: "Und noch einer.", post_id: 1},
+    {content: "Der hier gehört zum 2. Post.", post_id: 2}
 ])
