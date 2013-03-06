03b55e06075be346cac1f6188fc9ef7b3bc0d34b
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-06 15:55:41 +0100

    remove comments routes from router.js

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,14 +3,10 @@ EmberBlog.Router.map(function() {
     this.resource("posts", function() { // implicit path and template name (same as route name)
         //this.route('new');
     });
-    this.resource('post', { path: '/posts/:post_id' }, function() {
-        this.resource("comments", { path: "/comments" }, function() {
-            this.route("new");
-        });
-    });
+    this.resource('post', { path: '/posts/:post_id' });
     this.route("about");
 });
 
 EmberBlog.reopen({
     location: 'history'
-})
+});
