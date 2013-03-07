5c2807a66734188c331bc371db1e1aac9b6799ea
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-07 17:01:37 +0100

    remove superfluous file comments_route.js

--- app/assets/javascripts/routes/comments_route.js
+++
@@ -1,7 +0,0 @@
-EmberBlog.CommentRoute = Em.Route.extend({
-    model: function() {
-        Em.Logger.info("In PostComments Route");
-        var post = this.modelFor('post');
-        return EmberBlog.Comment.find(post.get('id'));
-    }
-});
