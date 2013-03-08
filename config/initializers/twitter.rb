b414c3970c5b278ded7e41197cafc3bcdfd224c1
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-08 16:00:30 +0100

    create twitter initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "wVtt09Ze4isf8laEyzrNw"
+  config.consumer_secret = "GBMr7i6oECtgfBbnVUZsiIh7YLurVpkPYi9rM2RUCk8"
+  config.oauth_token = "528586280-d1tC8xbr7ZaTCftBQnWOaFs8aySxeLakJVqfrBrG"
+  config.oauth_token_secret = "Mjqwty8WLHqHFRbVaQkYPn5dwD7KIAH6GFHrM2FGMM"
+end
