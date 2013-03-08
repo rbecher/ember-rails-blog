03953651850b0e33da306772bdba9a40737ca34c
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-08 17:48:54 +0100

    add method provider_name for prettifying provider names to the authentications helper

--- app/helpers/authentications_helper.rb
+++ app/helpers/authentications_helper.rb
@@ -1,2 +1,16 @@
 module AuthenticationsHelper
+
+  def provider_name(provider)
+    provider = provider.to_s
+    case provider
+      when 'open_id'
+        "OpenID"
+      when 'google_oauth2'
+        "Google"
+      when 'linkedin_oauth2'
+        "LinkedIn"
+      else
+        provider.titleize
+    end
+  end
 end
