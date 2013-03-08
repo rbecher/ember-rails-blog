fa26c230cf1be78abf8745d18b4dc1d0895e108d
Felix Saint-Paul <felix.saint-paul@wirtschaftswunderin.de>
2013-03-08 16:02:47 +0100

    add relationships and helper methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :authentications
+  has_many :posts
+  has_many :comments
+
+  def apply_omniauth(omniauth)
+    self.email = omniauth['user_info']['email'] if email.blank?
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end
+
+  def password_required?
+    (authentications.empty? || !password.blank?) && super
+  end
 end
